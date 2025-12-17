6789bf15ba5a3f7bb0ca286c1eacbcebd39044ce
manhnguyenvan2611 <manhnguyenvan2611@gmail.com>
2025-12-17 14:10:53 +0700

    add netlify.toml file

--- src/js/data/projects.js
+++ src/js/data/projects.js
@@ -1,46 +1,35 @@
 export const projects = [
   {
     id: 1,
-    name: "SHOES SHOP",
-    img: "shoes-shop.png",
+    name: "FACE RECOGNITION",
+    img: "photo-editor.jpg",
     time: "03/2025 - 04/2025",
     team: 1,
-    tech: "Reactjs, Spring, MySQL",
-    des: "text text text text text text text text text text text text text text text text",
-    src: "https://192.168.5.12",
-    demo: "https://192.168.5.12"
+    tech: "Django, SQLite, HTML, CSS, Javascript",
+    des: "A website login using face recognition by computing correlation of two images of face at at time. There are images of different user's faces stored in database already and i used cpanel for that. So when users login, they take a picture of themself using webcam, that picture they took will be compared with each image in the database. In this case, i use correlation formula for comparing. If the correlation is greate or equal than 0.7 then it's accepted and the users will be delivered to home page.",
+    src: "https://github.com/manhnguyenvan2611/face-recognition",
+    demo: "on going"
   },
   {
     id: 2,
-    name: "CHAP APP",
-    img: "project-management.png",
-    time: "03/2025 - 04/2025",
+    name: "PROJECT MANAGEMENT",
+    img: "project-management.jpg",
+    time: "06/2025 - 07/2025",
     team: 1,
-    tech: "Reactjs, Spring, MySQL",
-    des: "text text text text text text text text text text text text text text text text",
-    src: "https://192.168.5.12",
-    demo: "https://192.168.5.12"
+    tech: "Springboot, MySQL, HTML, CSS, Javascript",
+    des: "A website for managing projects. You can add description, status and time line and you also can edit and delete them. This is a simple project, there's only one table to store datas in database and the database is used is MySQL.",
+    src: "https://github.com/manhnguyenvan2611/project-management",
+    demo: "on going"
   },
   {
     id: 3,
-    name: "FOOD DELIVERY",
-    img: "project-management.png",
-    time: "03/2025 - 04/2025",
+    name: "PORTFOLIO",
+    img: "portfolio.jpg",
+    time: "07/2025 - 07/2025",
     team: 1,
-    tech: "Reactjs, Spring, MySQL",
-    des: "text text text text text text text text text text text text text text text text",
-    src: "https://192.168.5.12",
+    tech: "Reactjs",
+    des: "A simple portfolio in reactjs using yarn to initiate. This portfolio is just for showing off my skills.",
+    src: "https://github.com/manhnguyenvan2611/portfolio",
     demo: "https://192.168.5.12"
   },
-  {
-    id: 4,
-    name: "MUSIC APP",
-    img: "project-management.png",
-    time: "03/2025 - 04/2025",
-    team: 1,
-    tech: "Reactjs, Spring, MySQL",
-    des: "text text text text text text text text text text text text text text text text",
-    src: "https://192.168.5.12",
-    demo: "https://192.168.5.12"
-  }
 ];
